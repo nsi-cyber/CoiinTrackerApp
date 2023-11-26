3bb0a68d6810d84733d6c5f2d5c41b6ab4ebc0f4
nsi-cyber <enes.ay@dijitalsahne.com>
2023-11-26 21:03:45 +0300

    Notification and background service added, testing

---
+++ app/src/main/java/com/nsicyber/coiintrackerapp/service/BackgroundTrackerService.kt
@@ -0,0 +1,131 @@
+package com.nsicyber.coiintrackerapp.service
+
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.nsicyber.coiintrackerapp.R
+import com.nsicyber.coiintrackerapp.data.db.HawkUtils
+import com.nsicyber.coiintrackerapp.data.repository.FirebaseRepository
+import com.nsicyber.coiintrackerapp.data.repository.TrackerRepository
+import com.nsicyber.coiintrackerapp.model.CoinModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class BackgroundTrackerService : Service() {
+
+    private val handler = Handler()
+
+    @Inject
+    lateinit var trackerRepository: TrackerRepository
+
+    @Inject
+    lateinit var firebaseRepository: FirebaseRepository
+
+    @Suppress("DEPRECATION")
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+        return channelId
+    }
+
+    private fun createNotification(): Notification {
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "Coiin Tracker")
+            } else {
+                ""
+            }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Coiin Tracker")
+            .setContentText("Service working...")
+            .setSmallIcon(R.drawable.ic_logo) // Kendi ikonunuzu kullanın
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+
+        return notificationBuilder.build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "Servis oluşturuldu.")
+
+
+        val notification = createNotification()
+        startForeground(1, notification)
+
+
+
+        if (HawkUtils.getLastUser() != null)
+
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val myCoinsResult =
+                    firebaseRepository.readDataFromFirestore(HawkUtils.getLastUser())
+
+                myCoinsResult?.forEach { model ->
+                    model.toObject(CoinModel::class.java)?.reloadPerHour?.let { value ->
+                        startBackgroundTask(model.toObject(CoinModel::class.java))
+                    }
+
+                }
+
+
+            }
+
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun startBackgroundTask(model: CoinModel?) {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                GlobalScope.launch(Dispatchers.IO) {
+                    try {
+                        var notificationService = NotificationService(applicationContext)
+                        var responseModel = trackerRepository.getCoinDetail(model?.id!!)
+                        firebaseRepository.updateDataInFirestore(HawkUtils.getLastUser(),model?.id,responseModel!!)
+                        if (responseModel?.market_data?.current_price != model?.market_data?.current_price) {
+                            if ((responseModel?.market_data?.current_price?.usd
+                                    ?: 0).toDouble() > (model?.market_data?.current_price?.usd
+                                    ?: 0).toDouble()
+                            )
+                                notificationService.showNotification(model=responseModel,isUp = true)
+                            else
+                                notificationService.showNotification(model=responseModel,isUp = false)
+
+                        }
+
+
+                    } catch (e: Exception) {
+                        Log.e(TAG, "API isteği başarısız oldu:startBackgroundTask ${e.message}")
+                    }
+                }
+
+              //  handler.postDelayed(this, (model?.reloadPerHour?.toLong()?.times(360000) ?: 600000) )
+                handler.postDelayed(this, 10000 )
+            }
+
+        }, 0)
+    }
+
+    // Diğer kodlar...
+}
---
+++ app/src/main/java/com/nsicyber/coiintrackerapp/service/NotificationService.kt
@@ -0,0 +1,54 @@
+package com.nsicyber.coiintrackerapp.service
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.nsicyber.coiintrackerapp.MainActivity
+import com.nsicyber.coiintrackerapp.R
+import com.nsicyber.coiintrackerapp.model.CoinModel
+
+class NotificationService(
+    private val context: Context
+) : Service() {
+    private val notificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    fun showNotification(model: CoinModel?, isUp: Boolean?) {
+        val activityIntent = Intent(context, MainActivity::class.java)
+        val activityPendingIntent = PendingIntent.getActivity(
+            context,
+            1,
+            activityIntent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        )
+
+        val notification = NotificationCompat.Builder(context, TRACKER_CHANNEL)
+            .setSmallIcon(R.drawable.ic_logo)
+            .setContentTitle("Fiyat Değişti")
+            .setContentText(
+                if (isUp == true)
+                    "${model?.name} Coininizin fiyatı yükseldi! "
+                else
+                    "${model?.name} Coininizin fiyatı düştü! "
+
+
+            )
+            .setContentIntent(activityPendingIntent)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
+    companion object {
+        const val TRACKER_CHANNEL = "tracker_channel"
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
