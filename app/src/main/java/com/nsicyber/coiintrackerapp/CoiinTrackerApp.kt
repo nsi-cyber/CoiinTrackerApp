9c3574b39df418b4b31ebf62d0599ac1551ecb95
nsi-cyber <enes.ay@dijitalsahne.com>
2023-11-26 21:17:00 +0300

    Service connected to app. App Icon changed

--- app/src/main/java/com/nsicyber/coiintrackerapp/CoiinTrackerApp.kt
+++ app/src/main/java/com/nsicyber/coiintrackerapp/CoiinTrackerApp.kt
@@ -1,12 +1,21 @@
 package com.nsicyber.coiintrackerapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.content.ContextCompat
 import com.google.firebase.FirebaseApp
+import com.nsicyber.coiintrackerapp.service.BackgroundTrackerService
+import com.nsicyber.coiintrackerapp.service.NotificationService
 import com.orhanobut.hawk.Hawk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CoiinTrackerApp : Application() {
+
 
     override fun onCreate() {
 
@@ -15,7 +24,29 @@ class CoiinTrackerApp : Application() {
         if (!Hawk.isBuilt()) {
             Hawk.init(this).build()
         }
+        createNotificationChannel()
+        val serviceIntent = Intent(this, BackgroundTrackerService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(this, serviceIntent)
+        } else {
+            this.startService(serviceIntent)
+        }
     }
 
 
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NotificationService.TRACKER_CHANNEL,
+                "Tracker",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "Used for the notifications"
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 }
